3ea7ec8524ca7ebc465d655d186b14cc27e23ba7
oleg <olegs777@bezeqint.net>
2019-06-11 18:44:24 +0300

    ok

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,19 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { Provider } from "react-redux";
+import store from "./store";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>This is my test app</p>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>This is my test app</p>
+        </header>
+      </div>
+    </Provider>
   );
 }
 
--- client/src/store.js
+++ client/src/store.js
@@ -1,13 +1,13 @@
-import {} from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
-// import rootReducer from "./reducers";
+import rootReducer from "./reducers";
 
 const initialState = {};
 
 const middleware = [thunk];
 
 const store = createStore(
-  {},
+  rootReducer,
   initialState,
   compose(
     applyMiddleware(...middleware),

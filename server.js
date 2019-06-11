d93e9b07126c0eb462ddd4bfee541c40c19bb3bc
oleg <olegs777@bezeqint.net>
2019-06-11 11:44:06 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+// Server Static Assets while in production
+if (process.env.NODE_ENV === "production") {
+  // set static folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+const port = process.env.PORT || 5000;
+const server = app.listen(port, () =>
+  console.log(`Server running on port${port}`)
+);
